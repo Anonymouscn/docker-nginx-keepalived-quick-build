d80df2f5e5031bcce4da3edc3c5207088a2ccf4a
Anonymouscn <pgl888999@icloud.com>
2024-12-15 17:14:01 +0800

    适配带有 keepalived 主备切换的 nginx 1.26.1, 1.26.2, 1.27.3 在 docker 中的构建

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker nginx with keepalived quick build script
+
+A project to quickly build and deploy nginx with keepalived on docker !
---
+++ ubuntu_base/latest/build.sh
@@ -0,0 +1,8 @@
+# 选择搭建 keepalive 工作节点或备用节点
+cp ../../keepalived-master.conf tmp/keepalived.conf
+# 拷贝 entrypoint 启动文件
+cp ../../entrypoint.sh tmp/entrypoint.sh
+# 交叉编译构建镜像
+docker buildx build --platform linux/amd64,linux/arm64 -t pgl888999/nginx-with-keepalived-in-docker:latest --push .
+# 清除临时产物
+rm -rf tmp/*
